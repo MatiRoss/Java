c276fbb8afbd92cfb355857f5707f9a1478087c9
MatiRoss <mati04.ross@gmail.com>
2021-04-27 14:33:52 +0200

    a

--- src/demenagement/Demenagement.java
+++ src/demenagement/Demenagement.java
@@ -1,17 +1,24 @@
 package demenagement;
 
+import java.util.Scanner;
+
 public class Demenagement {
 
 	public static void main(String[] args) {
-		int local = 34;
+		Scanner clavier = new Scanner(System.in);
+		System.out.print("Saisir le nombre de cartons");
+		int local = clavier.nextInt();
+		
+		int capaciteCamion = 9;
 		int camion = 0;
 		int newLocal = 0;
+		clavier.close();
 		
 		while (local != 0) {
 			local = local -1;
 			camion = camion +1;
 			newLocal = newLocal +1;
-			if (camion == 9) {
+			if (camion == capaciteCamion) {
 				System.out.println("Un voyage de " + camion + " cartons");
 				camion = 0;
 			}
