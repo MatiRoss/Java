8b66721b2347740080ae48ed0b36a754410274fa
MatiRoss <mati04.ross@gmail.com>
2021-04-27 12:06:33 +0200

    Déménagement

---
+++ src/demenagement/Demenagement.java
@@ -0,0 +1,27 @@
+package demenagement;
+
+public class Demenagement {
+
+	public static void main(String[] args) {
+		int local = 34;
+		int camion = 0;
+		int newLocal = 0;
+		
+		while (local != 0) {
+			local = local -1;
+			camion = camion +1;
+			newLocal = newLocal +1;
+			if (camion == 9) {
+				System.out.println("Un voyage de " + camion + " cartons");
+				camion = 0;
+			}
+			
+		}
+		
+		System.out.println("Un voyage de " + camion + " cartons");
+		System.out.println("L'ancien local contient " + local + " cartons");
+		System.out.println("Le nouveau local contient " + newLocal + " cartons");
+
+	}
+
+}
